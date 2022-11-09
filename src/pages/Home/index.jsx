a5570533d94a59fd76c53b2b7761a35c1fcb8ff1
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-11-08 22:47:44 -0300

    feat install styled-components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ export function Home(params) {
             src="https://s2.glbimg.com/TxQm7TbPjtdv99GcVGFgroItgxU=/e.glbimg.com/og/ed/f/original/2021/11/16/hasvc_cartaz.posted_1080x1350px_data.jpg"
             alt="Spider Man"
           />
-          <span></span>
+          <span>Spider Man</span>
         </li>
         <img
           src="https://s2.glbimg.com/TxQm7TbPjtdv99GcVGFgroItgxU=/e.glbimg.com/og/ed/f/original/2021/11/16/hasvc_cartaz.posted_1080x1350px_data.jpg"
