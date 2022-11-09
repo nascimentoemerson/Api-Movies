7be7c61f82dfc70c321fd083af27400ec4485434
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-11-08 22:45:41 -0300

    feat global and main

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,34 @@
 export function Home(params) {
-    return (
-        <h1>Olá Mundo</h1>
-    )
+  return (
+    <div>
+      <h1>Mega Movies</h1>
+      <ul>
+        <li>
+          <img
+            src="https://s2.glbimg.com/TxQm7TbPjtdv99GcVGFgroItgxU=/e.glbimg.com/og/ed/f/original/2021/11/16/hasvc_cartaz.posted_1080x1350px_data.jpg"
+            alt="Spider Man"
+          />
+          <span></span>
+        </li>
+        <img
+          src="https://s2.glbimg.com/TxQm7TbPjtdv99GcVGFgroItgxU=/e.glbimg.com/og/ed/f/original/2021/11/16/hasvc_cartaz.posted_1080x1350px_data.jpg"
+          alt=""
+        />
+        <span></span>
+        <li>
+          <img
+            src="https://s2.glbimg.com/TxQm7TbPjtdv99GcVGFgroItgxU=/e.glbimg.com/og/ed/f/original/2021/11/16/hasvc_cartaz.posted_1080x1350px_data.jpg"
+            alt=""
+          />
+          <span></span>
+        </li>
+        <img src="" alt="" />
+        <span></span>
+        <li>
+          <img src="" alt="" />
+          <span></span>
+        </li>
+      </ul>
+    </div>
+  );
 }
